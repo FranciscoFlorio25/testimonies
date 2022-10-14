bff3c6dea3b83e1e94fed7702b072cd45831d5f7
Francisco Florio <fflorio@algeiba.com>
2022-10-14 13:43:22 -0300

    primer testimonio estatico

---
+++ src/Components/Testimonies.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Testimonie() {
+  return(
+    <div className='container-testimonie'>
+      <img 
+      className='image-testimonie'
+      src={require('../Image/placeHolder.jpg')}
+      alt = 'foto de dador de testimonio'
+      />
+      <div className='container-testimonie-text'>
+        <p className='testimonie-name'>Juancito Rodigues en Argentina</p>
+        <p className='role-testimonie-'>Ingeniero de Software en TurtleSoft</p>
+        <p className='testimonie-text'>Yo era un aventurero, viaje de aqui a aca,
+         hasta que aprendi react, ahora, soy un desarrollador hecho y derecho</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonie;
+
